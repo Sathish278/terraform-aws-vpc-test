df265e856740b9040092fa1a70a161072341889d
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-05-23 20:50:28 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "5.48.0"
     }
   }
-  backend "s3" {
-    bucket = "sathishreddy-remote-state"
-    key    = "expense-vpc"
-    region = "us-east-1"
-    dynamodb_table = "sathishreddy-locking"
-  }
+#   backend "s3" {
+#     bucket = "sathishreddy-remote-state"
+#     key    = "expense-vpc"
+#     region = "us-east-1"
+#     dynamodb_table = "sathishreddy-locking"
+#   }
 }
 
 #provide authentication here
