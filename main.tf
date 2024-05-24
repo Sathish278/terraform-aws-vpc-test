8bfb5cf1961c2d0ba06fdea03af0c5d26a7964a9
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-05-24 22:31:20 +0530

    final

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/Sathish278/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
